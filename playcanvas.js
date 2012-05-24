b59c54e41aa3f8a521a7603f813e401c790d9bf4
Tom MacWright <tom@macwright.org>
2012-05-24 13:13:55 -0400

    Cleanup

--- playcanvas.js
+++ playcanvas.js
@@ -1,103 +1,122 @@
 function playcanvas(canvas) {
-  if (!window.webkitAudioContext) {
-    throw new Exception('webkitAudioContext not available');
-  }
-
-  var p = {},
-      audio_ctx,
-      canvas_ctx,
-      canvas_data,
-      processor,
-      note_time,
-      nindex = 0;
-
-  var PI2 = Math.PI * 2,
-      phase = 0.0,
-      baseFrequency = 440.0,
-      sampleRate = 44100.0,
-      bufferSize = 2048, // must be power of 2
-      phaseIncrement = PI2 * baseFrequency / sampleRate,
-      ctx, jsProcessor,
-      soundEnabled = false;
-
-  // This function will be called repeatedly to fill an audio buffer and
-  // generate sound.
-  function process(event) {
-      // Get array associated with the output port.
-      var outputArray = event.outputBuffer.getChannelData(0);
-      var n = outputArray.length;
-
-      if (soundEnabled) {
-          for (var i = 0; i < n; ++i) {
-              // Generate a sine wave.
-              var sample = Math.sin(phase);
-              outputArray[i] = sample * 0.6;
-              // Increment and wrap phase.
-              phase += phaseIncrement;
-              if (phase > PI2) {
-                  phase -= PI2;
-              }
-          }
-      } else {
-          // Output silence.
-          for (var i = 0; i < n; ++i) {
-              outputArray[i] = 0.0;
-          }
-      }
-  }
-
-  function initialize_audio() {
-    audio_ctx = new webkitAudioContext(),
-    processor = audio_ctx.createJavaScriptNode(2048, 0, 1);
-    processor.onaudioprocess = process;
-    processor.connect(audio_ctx.destination);
-  }
-
-  function initialize_canvas() {
-    canvas_ctx = canvas.getContext('2d');
-    canvas_data = canvas_ctx.getImageData(0, 0, canvas.width, canvas.height);
-  }
-
-  var canvas_width = 200, canvas_height = 30;
-
-  function getpx(data, x, y) {
-     var r = data[4 * ((y * canvas.width) + x) + 0],
-         g = data[4 * ((y * canvas.width) + x) + 1],
-         b = data[4 * ((y * canvas.width) + x) + 2],
-         a = data[4 * ((y * canvas.width) + x) + 3];
-    return [r, g, b, a];
-  }
-
-  var flippedpx;
-  function flippx(ctx, data, x, y) {
-    if (flippedpx) {
-      ctx.fillStyle = 'rgba(' + flippedpx[2].join(',') + ')';
-      ctx.fillRect(flippedpx[0], flippedpx[1], 1, 1);
+    if (!window.webkitAudioContext) {
+        throw new Exception('webkitAudioContext not available');
     }
-    flippedpx = [x, y, getpx(data, x, y)];
-    ctx.fillStyle = '#000';
-    ctx.fillRect(x, y, 1, 1);
-  }
 
-  function play_note() {
-    soundEnabled = true;
-    var px = getpx(canvas_data.data, nindex % canvas.width, Math.floor(nindex / canvas.width));
-    flippx(canvas_ctx, canvas_data.data, nindex % canvas.width, Math.floor(nindex / canvas.width));
-    phaseIncrement = PI2 * (px[0] + px[1] + px[2]) / sampleRate;
-    nindex++;
-    if (nindex < (canvas.width * canvas.height)) {
-      note_time = window.setTimeout(play_note, 0);
-    } else {
-      soundEnabled = false;
+    var p = {},
+        audio_ctx,
+        canvas_ctx,
+        canvas_data,
+        processor,
+        note_time,
+        nindex = 0;
+
+    var PI2 = Math.PI * 2,
+        phase = 0.0,
+        baseFrequency = 440.0,
+        sampleRate = 44100.0,
+        bufferSize = 2048, // must be power of 2
+        phaseIncrement = PI2 * baseFrequency / sampleRate,
+        ctx, jsProcessor,
+        soundEnabled = false;
+
+    // This function will be called repeatedly to fill an audio buffer and
+    // generate sound.
+    function process(event) {
+        // Get array associated with the output port.
+        var outputArray = event.outputBuffer.getChannelData(0);
+        var n = outputArray.length;
+        var i;
+
+        if (soundEnabled) {
+            for (i = 0; i < n; ++i) {
+                // Generate a sine wave.
+                var sample = Math.sin(phase);
+                outputArray[i] = sample * 0.6;
+                // Increment and wrap phase.
+                phase += phaseIncrement;
+                if (phase > PI2) {
+                    phase -= PI2;
+                }
+            }
+        } else {
+            // Output silence.
+            for (i = 0; i < n; ++i) {
+                outputArray[i] = 0.0;
+            }
+        }
     }
-  }
 
-  p.play = function() {
-    note_time = play_note();
-  }
+    // Create a new audio context. This should be called only once.
+    function initialize_audio() {
+        audio_ctx = new webkitAudioContext(),
+        processor = audio_ctx.createJavaScriptNode(2048, 0, 1);
+        processor.onaudioprocess = process;
+        processor.connect(audio_ctx.destination);
+    }
 
-  initialize_audio();
-  initialize_canvas();
+    // Create a new canvas context. This should be called only once.
+    function initialize_canvas() {
+        canvas_ctx = canvas.getContext('2d');
+        canvas_data = canvas_ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
 
-  return p;
+    function getpx(data, x, y) {
+        var r = data[4 * ((y * canvas.width) + x) + 0],
+        g = data[4 * ((y * canvas.width) + x) + 1],
+        b = data[4 * ((y * canvas.width) + x) + 2],
+        a = data[4 * ((y * canvas.width) + x) + 3];
+        return [r, g, b, a];
+    }
+
+    var flippedpx;
+    function flippx(ctx, data, x, y) {
+        if (flippedpx) {
+            ctx.fillStyle = 'rgba(' + flippedpx[2].join(',') + ')';
+            ctx.fillRect(flippedpx[0], flippedpx[1], 1, 1);
+        }
+        flippedpx = [x, y, getpx(data, x, y)];
+        ctx.fillStyle = '#000';
+        ctx.fillRect(x, y, 1, 1);
+    }
+
+    function play_note() {
+        soundEnabled = true;
+        var px = getpx(canvas_data.data, nindex % canvas.width, Math.floor(nindex / canvas.width));
+        flippx(canvas_ctx,
+            canvas_data.data,
+            nindex % canvas.width,
+            Math.floor(nindex / canvas.width));
+        phaseIncrement = PI2 * (px[0] + px[1] + px[2]) / sampleRate;
+        nindex++;
+        if (nindex < (canvas.width * canvas.height)) {
+            note_time = window.setTimeout(play_note, 0);
+        } else {
+            // Stop any audio output
+            soundEnabled = false;
+            // Rewind to the beginning.
+            nindex = 0;
+        }
+    }
+
+    // Play a note
+    p.play = function() {
+        note_time = play_note();
+    };
+
+    // Pause playing
+    p.pause = function() {
+        window.clearTimeout(note_time);
+    };
+
+    // Stop playing
+    p.stop = function() {
+        p.pause();
+        nindex = 0;
+    };
+
+    initialize_audio();
+    initialize_canvas();
+
+    return p;
 }
